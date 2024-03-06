d7fb329d454535db40a1cb89a28edcbe2a26dac4
genos322 <genos322@gmail.com>
2024-03-06 01:13:26 -0500

    Feat: pasando pruebas

--- src/analyzer.js
+++ src/analyzer.js
@@ -9,22 +9,55 @@ export const analyzer = {
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    return text.replace(/[\s]/g, '').length;
+    const characters = text.replace(/[.,;:!?(){}\s]/g, '');
+    return characters.length;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
     const totalCharacter = text.replace(/\s/g,'').length
     const totalWords = text.split(/\s+/).length
-    return Math.round(totalCharacter/totalWords);
+    return parseFloat((totalCharacter/totalWords).toFixed(2));
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    const arr = text.match(/\b\d+\b/g) ?? [];
-    return arr.length;
+    // const arr = text.match(/\b\d+(\.\d+)?\b/g);
+    // if(arr===null)
+    // {
+    //   return 0;
+    // }
+    // return arr.length;
+    const numbers = text.split(' ').filter(word => {
+      if (word.endsWith('.')) {
+        word = word.slice(0, -1);
+      }
+      return !isNaN(word) && word.trim() !== '';
+    }).length;
+    if(numbers===null)
+    { 
+      return 0;
+    }
+    return numbers;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    const arr = text.match(/\b\d+\b/g) ?? [];["2","3"]
-    return arr.reduce((a, next) => a + parseInt(next),0);
+    // let numerosArray = text.match(/\b\d+(\.\d+)?\b/g);
+    // if(numerosArray===null)
+    // {
+    //   numerosArray = [];
+    // }
+    // let suma = 0;
+    // for (let i = 0; i < numerosArray.length; i++) {
+    //   suma += parseFloat(numerosArray[i]);
+    // }
+    // return suma;
+    const numbers = text.split(' ').filter(word => {
+      if (word.endsWith('.')) {
+        word = word.slice(0, -1);
+      }
+      return !isNaN(word) && word.trim() !== '';
+    })
+      .map(number => parseFloat(number))
+      .reduce((acc, number) => acc + number, 0);
+    return numbers;
   },
 };
